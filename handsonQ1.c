afd2c52159d3513e0ccc88d4c1a8893683aea421
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-08-11 22:02:25 +0530

    handson Q1

--- handsonQ1.c
+++ handsonQ1.c
@@ -1,26 +1,30 @@
+// including important header file like stdio for i/o and 
+// stdlib for memory management (sys/types,sys/stat,fcntl) for file and directory management
+// and for system call unistd
 #include<stdio.h>
+#include<unistd.h>
 #include<stdlib.h>
-#include<sys/types.h>
-#include<sys/stat.h>
+#include<sys/types.h>//mainly used for fifo
+#include<sys/stat.h>// mainly used for fifo
 #include<fcntl.h>
-
 int main()
 {
-	// take input from command line
-	int v = symlink("dest","destSL");
-	if(v<0)
+	// creating a soft link
+	if (symlink("my_file.txt","sl")<0) //it create a softlink
 	{
-		perror("failed");return 1;	
+		perror("error in creating a soft link\n");//this function will print specific error
+		return 1;
 	}
-	int f = link("dest","destHL");
-	if(f<0)
+	if (link("my_file.txt","hl")<0)	// it will create a hardlink
 	{
-		perror("failed");return 1;
+		perror("error in creating a hard link\n");
+		return 1;
 	}
-	int e = mknod("destFIFO",S_IFIFO,0);
-        if(e<0)
+	if (mknod("my_file1.txt",S_IFIFO,0)<0)
 	{
-		perror("failed");return 1;
-	}	
+		perror("error in creating a Fifo\n");
+		return 1;
+	}
+	printf("softlink, hardlink, fifo created successfully.\n");
 	return 0;
 }
