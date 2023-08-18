dcf785f78753aa6f086ab226a014e1694dfbc502
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-08-18 10:56:59 +0530

    question 6 and 7

---
+++ handsonQ6.c
@@ -0,0 +1,11 @@
+// 6. Write a program to take input from STDIN and display on STDOUT. Use only read/write system calls
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a;
+    printf("enter input\n");
+    scanf("%d",&a);
+    printf("ans: %d",a);
+    return 0;
+}
