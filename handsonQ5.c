994f247d485400d13f85e71c554365c6ce2315dc
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-08-22 00:32:47 +0530

    question 5 update

--- handsonQ5.c
+++ handsonQ5.c
@@ -3,9 +3,21 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<fcntl.h>
-
 int main()
 {
-    int file_descriptor=open('fileQ51',O_RDWR|O_CREAT|O_EXCL);
+    // Create five new files
+    int fd1 = open("fileQ5a",O_RDWR|O_CREAT|O_EXCL, 0744);
+    printf("File descriptor 1: %d\n",fd1);
+    int fd2 = open("fileQ5b",O_RDWR|O_CREAT|O_EXCL, 0744);
+    printf("File descriptor 2: %d\n",fd2);
+    int fd3 = open("fileQ5c",O_RDWR|O_CREAT|O_EXCL, 0744);
+    printf("File descriptor 3: %d\n",fd3);
+    int fd4 = open("fileQ5d",O_RDWR|O_CREAT|O_EXCL, 0744);
+    printf("File descriptor 4: %d\n",fd4);
+    int fd5 = open("fileQ5e",O_RDWR|O_CREAT|O_EXCL, 0744);
+    printf("File descriptor 5: %d\n",fd5);
+
+    for(;;);
+
     return 0;
 }
