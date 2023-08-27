cb753d649dfc47531286cc51c5eed882c90ed1d5
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-08-27 10:39:13 +0530

    update

--- handsonQ2.c
+++ handsonQ2.c
@@ -6,23 +6,10 @@
 #include<sys/types.h>
 int main()
 {
-	pid_t pid=fork();//pid_t is a data type use for used to store pid in it.
-	if (pid<0)
-	{
-		perror("fork failed.\n");
-		return 1;
-	}
-	if (pid==0)
-	{	//child process will run for inf
+		//child process will run for inf
 		while(1)
 		{
-
+			
 		}
-	}
-	else
-	{
-		system("ls -d /proc/[0-9]*");
-		return 0;
-	}
 	return 0;
 }
