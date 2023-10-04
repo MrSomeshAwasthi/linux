e4cb05425aef84c33434f37e932e07ebecccde8f
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-10-05 02:08:16 +0530

    Q14

--- handsonlist2/Q14/14.c
+++ handsonlist2/Q14/14.c
@@ -1,32 +1,44 @@
 /*
-14. Write a simple program to create a pipe, write to the pipe, read from pipe and display on
-the monitor
+==================================================================================================
+Name : 14.c
+Author : Somesh Awasthi
+Description :Write a simple program to create a pipe, write to the pipe, read from pipe and 
+display on the monitor
+Date: 28th Aug, 2023.
+==================================================================================================
 */
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
-#include<string.h>
-int main(int argc , char *argv[])
+#include<fcntl.h>
+#include<string.h>  
+int main()
 {
     int pipefd[2];
-    char buff;
-    if (argc!=2)
+    char buff[100];
+    int pipes=pipe(pipefd);
+    if(pipes==-1)
     {
+        perror("error in creating pipe");
         return 0;
     }
-    if (pipe(pipefd)<0)
-        {
-            perror("pipe not created");
-            return 0;
-        }
-    close(pipefd[1]);
-    while (read(pipefd[0],&buff,1)>0)
+    printf("enter data: ");
+    fgets(buff,sizeof(buff),stdin);
+    close(pipefd[0]);
+    printf("write");
+    if(write(pipefd[1],buff,sizeof(buff))==-1)
     {
-        write(STDOUT_FILENO,&buff,1);
+        perror("error in writing");
+        return 0;
+    }
+    printf("write success");
+    close(pipefd[1]);
+    printf("read");
+    if(read(pipefd[0],buff,sizeof(buff))==-1)
+    {
+        perror("error in reading");
+        return 0;
     }
     close(pipefd[0]);
-    write(1,argv[1],strlen(argv[1]));
-    close(pipefd[1]);    
-    printf("\n");
+    printf("read success: %s",buff);
     return 0;
 }
