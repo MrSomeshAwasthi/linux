1a72e3d37c7ece0164e78400e70194478b14843c
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-10-12 07:45:55 +0530

    update

--- project/client.c
+++ project/client.c
@@ -2,60 +2,132 @@
     AUTHOR : SOMESH AWASTHI
     FILE DESCRIPTION : 
 */
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<netinet/ip.h>
-int main()
-{
-    char send_buf[1024],recv_buf[1024];
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-    //specifying server ip and port
+#define BUF_SIZE 1024
+
+int main() {
+    char send_buf[BUF_SIZE], recv_buf[BUF_SIZE];
+
+    // Server address and port
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(12345);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.7");
-    
-    // socket connection
-    int client_socket=socket(AF_INET,SOCK_STREAM,0);
-    if(client_socket==-1)
-    {
-        perror("client socket error.\n");
+    server_address.sin_port = htons(8080); // Change to the server's port (8080 in this case)
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Change to the server's IP address
+
+    // Socket connection
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Client socket error.\n");
         exit(1);
     }
-    
-    // client connect
-    int client_connect=connect(client_socket,(struct sockaddr *)&server_address,sizeof(server_address));
-    if(client_connect==-1)
-    {
-        perror("client connect error.\n");
+
+    // Client connect
+    int client_connect = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (client_connect == -1) {
+        perror("Client connect error.\n");
         exit(1);
     }
-    while (1)
-    {    
-        //  communicating with server
+
+    while (1) {
+        // Communicating with the server
         printf("C: ");
-        fgets(send_buf,sizeof(send_buf),stdin);
+        fgets(send_buf, sizeof(send_buf), stdin);
         int byte_sent = send(client_socket, send_buf, strlen(send_buf), 0);
-        if (byte_sent < 0)
-        {
-            perror("error sending data");
-            exit(1);
-        }
-        
-        memset(recv_buf,0,sizeof(recv_buf));
-        int byte_received = recv(client_socket, recv_buf, sizeof(recv_buf), 0);
-        if (byte_received < 0)
-        {
-            perror("error receiving data");
+        if (byte_sent < 0) {
+            perror("Error sending data");
             exit(1);
         }
 
-        printf("S: %s",recv_buf);
+        memset(recv_buf, 0, sizeof(recv_buf));
+        int byte_received = recv(client_socket, recv_buf, sizeof(recv_buf), 0);
+        if (byte_received < 0) {
+            perror("Error receiving data");
+            exit(1);
+        }
+
+        printf("S: %s", recv_buf);
     }
+
     close(client_socket);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// #include<unistd.h>
+// #include<arpa/inet.h>
+// #include<sys/socket.h>
+// #include<netinet/ip.h>
+// int main()
+// {
+//     char send_buf[1024],recv_buf[1024];
+
+//     //specifying server ip and port
+//     struct sockaddr_in server_address;
+//     server_address.sin_family = AF_INET;
+//     server_address.sin_port = htons(8080);
+//     server_address.sin_addr.s_addr = inet_addr("127.0.0.7");
+    
+//     // socket connection
+//     int client_socket=socket(AF_INET,SOCK_STREAM,0);
+//     if(client_socket==-1)
+//     {
+//         perror("client socket error.\n");
+//         exit(1);
+//     }
+    
+//     // client connect
+//     int client_connect=connect(client_socket,(struct sockaddr *)&server_address,sizeof(server_address));
+//     if(client_connect==-1)
+//     {
+//         perror("client connect error.\n");
+//         exit(1);
+//     }
+//     while (1)
+//     {    
+//         //  communicating with server
+//         printf("C: ");
+//         fgets(send_buf,sizeof(send_buf),stdin);
+//         int byte_sent = send(client_socket, send_buf, strlen(send_buf), 0);
+//         if (byte_sent < 0)
+//         {
+//             perror("error sending data");
+//             exit(1);
+//         }
+        
+//         memset(recv_buf,0,sizeof(recv_buf));
+//         int byte_received = recv(client_socket, recv_buf, sizeof(recv_buf), 0);
+//         if (byte_received < 0)
+//         {
+//             perror("error receiving data");
+//             exit(1);
+//         }
+
+//         printf("S: %s",recv_buf);
+//     }
+//     close(client_socket);
+//     return 0;
+// }
