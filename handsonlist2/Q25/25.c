daa761c4860a4a478ceecc62eae9e1412e24f3e2
MrSomeshAwasthi <someshawasthi31@gmail.com>
2023-10-20 00:13:16 +0530

    till Q25

---
+++ handsonlist2/Q25/25.c
@@ -0,0 +1,16 @@
+/*
+==================================================================================================
+Name : 25.c
+Author : Somesh Awasthi
+Description : Write a program to print a message queue's  (use msqid_ds and ipc_perm structures)
+                a. access permission   
+                b. uid, gid   
+                c. time of last message sent and received       
+                d. time of last change in the message queue    
+                e. size of the queue    
+                f. number of messages in the queue     
+                g. maximum number of bytes allowed     
+                h. pid of the msgsnd and msgrcv 
+Date: 1st oct, 2023.
+==================================================================================================
+*/
